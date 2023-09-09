e40cfa19878f4bda7830f4b1312c48fa0fbc93e2
Ravi Kiran <ravi.kiran8844@gmail.com>
2023-09-09 10:05:15 +0530

    updated code

---
+++ assets/products-slider.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        responsiveClass: true,
+        margin:10,
+        nav: true,
+        navText : ["<img src='https://cdn.shopify.com/s/files/1/0551/8157/4300/files/prev-img.png' style='width:40px;height:40px;'>","<img style='width:40px;height:40px;' src='https://cdn.shopify.com/s/files/1/0551/8157/4300/files/next-img.png'>"],
+        center: true,
+        responsive:{
+            0: {
+                items: 1
+            },
+            768: {
+                items: 3
+
+            },
+            1240: {
+                items: 3
+            }
+        }
+    });
+  });
