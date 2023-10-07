c4d7a69c8fa95b374454005202cc428a766e939e
Ravi Kiran <ravi.kiran8844@gmail.com>
2023-10-07 22:01:28 +0530

    updated theme

--- assets/nasal-spray-facts.js
+++ assets/nasal-spray-facts.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
-    $("#products-slider").owlCarousel({
+    $("#nasal-spray-facts-slider").owlCarousel({
         loop:true,
         responsiveClass: true,
         margin:10,
         nav: true,
         navText : ["<img src='https://cdn.shopify.com/s/files/1/0551/8157/4300/files/prev-img.png' style='width:40px;height:40px;'>","<img style='width:40px;height:40px;' src='https://cdn.shopify.com/s/files/1/0551/8157/4300/files/next-img.png'>"],
-        center: true,
         responsive:{
             0: {
                 items: 1
             },
             768: {
-                items: 3
+                items: 2    
 
             },
             1240: {
-                items: 3
+                items: 2
             }
         }
     });
